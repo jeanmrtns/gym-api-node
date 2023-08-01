2de37b87730b1e8812d43b83e5d5262d5373065f
Jean Martins <jeanmrtns4@gmail.com>
2023-08-01 16:43:18 -0300

    feat: validate if user exists and hash password

--- src/http/controllers/register-user.controller.ts
+++ src/http/controllers/register-user.controller.ts
@@ -1,4 +1,5 @@
 import { prisma } from '@/infra/database/prisma';
+import { hash } from 'bcryptjs';
 import { FastifyReply, FastifyRequest } from 'fastify';
 import { z } from 'zod';
 
@@ -14,11 +15,23 @@ export async function registerUserController(
 
   const { name, email, password } = registerUserSchema.parse(request.body);
 
+  const userWithSameEmail = await prisma.user.findUnique({
+    where: {
+      email,
+    },
+  });
+
+  if (userWithSameEmail) {
+    return reply.status(409).send();
+  }
+
+  const passwordHash = await hash(password, 6);
+
   await prisma.user.create({
     data: {
       name,
       email,
-      password_hash: password,
+      password_hash: passwordHash,
     },
   });
 
