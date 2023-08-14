b869a7c9708b12574074320b80589c4eac3a5e13
Jean Martins <jeanmrtns4@gmail.com>
2023-08-14 20:48:38 -0300

    test: create register user e2e test

--- src/http/controllers/register-user.controller.spec.ts
+++ src/http/controllers/register-user.controller.spec.ts
@@ -1,7 +1,23 @@
-import { describe, expect, it } from 'vitest';
+import request from 'supertest';
+import { app } from '@/app';
+import { afterAll, beforeAll, describe, expect, it } from 'vitest';
 
 describe('Register user controller', () => {
-  it('should ok', () => {
-    expect(true).toBe(true);
+  beforeAll(async () => {
+    await app.ready();
+  });
+
+  afterAll(async () => {
+    await app.close();
+  });
+
+  it('should be able to create a new user', async () => {
+    const response = await request(app.server).post('/users').send({
+      name: 'John Doe',
+      email: 'john@doe.com',
+      password: '123456',
+    });
+
+    expect(response.statusCode).toEqual(201);
   });
 });
